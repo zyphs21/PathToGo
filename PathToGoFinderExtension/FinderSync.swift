d693cccc9b2eee480e48cb374e958c7e209647ca
zyphs21 <hansenhs21@live.com>
2020-06-11 20:47:03 +0800

    Add PathToGoExtension Target

---
+++ PathToGoFinderExtension/FinderSync.swift
@@ -0,0 +1,86 @@
+//
+//  FinderSync.swift
+//  PathToGoFinderExtension
+//
+//  Created by Hanson on 2020/6/11.
+//  Copyright © 2020 HansonStudio. All rights reserved.
+//
+
+import Cocoa
+import FinderSync
+
+class FinderSync: FIFinderSync {
+    
+    override init() {
+        super.init()
+    }
+    
+    override var toolbarItemName: String {
+        return "PathToGo"
+    }
+    
+    override var toolbarItemToolTip: String {
+        return "PathToGo: Click to Copy the current directory's path to the clipboard"
+    }
+    
+    override var toolbarItemImage: NSImage {
+        let icon = NSImage(named: "PathToGoExtension")
+        icon?.isTemplate = true
+        return icon!
+    }
+    
+    override func menu(for menuKind: FIMenuKind) -> NSMenu {
+        let menu = NSMenu(title: "")
+        menu.addItem(withTitle: "Copy The Path", action: #selector(copyPlainText(_:)), keyEquivalent: "")
+        menu.addItem(withTitle: "Copy The Path With Backslash Escaping For Space", action: #selector(copySpaceBackslashText(_:)), keyEquivalent: "")
+        return menu
+    }
+    
+    @IBAction func copyPlainText(_ sender: AnyObject?) {
+        guard let target = FIFinderSyncController.default().targetedURL()
+            , let selectedItems = FIFinderSyncController.default().selectedItemURLs() else { return }
+        
+        var itemURLString = ""
+        for (index, item) in selectedItems.enumerated() {
+            itemURLString = itemURLString + item.path
+            if index != selectedItems.count - 1 {
+                itemURLString = itemURLString + "\n"
+            }
+        }
+        
+        // When Only select the target folder, the selectedItems is empty
+        // 仅仅选中 Target fodler 的时候 selectedItems 是空的
+        if itemURLString.count == 0 {
+            itemURLString = target.path
+        }
+        
+        let pasteboard = NSPasteboard.general
+        pasteboard.declareTypes([.string], owner: nil)
+        pasteboard.setString("\(itemURLString)", forType: .string)
+    }
+    
+    @IBAction func copySpaceBackslashText(_ sender: AnyObject?) {
+        guard let target = FIFinderSyncController.default().targetedURL()
+            , let selectedItems = FIFinderSyncController.default().selectedItemURLs() else { return }
+        
+        var itemURLString = ""
+        for (index, item) in selectedItems.enumerated() {
+            itemURLString = itemURLString + item.path
+            if index != selectedItems.count - 1 {
+                itemURLString = itemURLString + "\n"
+            }
+        }
+        
+        // When Only select the target folder, the selectedItems is empty
+        // 仅仅选中 Target fodler 的时候 selectedItems 是空的
+        if itemURLString.count == 0 {
+            itemURLString = target.path
+        }
+        
+        let finalString = itemURLString.replacingOccurrences(of: " ", with: "\\ ")
+        let pasteboard = NSPasteboard.general
+        pasteboard.declareTypes([.string], owner: nil)
+        pasteboard.setString("\(finalString)", forType: .string)
+    }
+}
+
